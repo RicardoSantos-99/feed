d8f55c5c7588e2ee8184172b00e21c71a1a2e560
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-09-28 23:45:39 -0300

    feat: finish sidebar style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -6,12 +7,19 @@ export function Sidebar() {
       <img src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/RicardoSantos-99.png"
+        />
         <strong>John Doe</strong>
         <span>React Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
